e23cc755c988dca531bf6997925d875aa41f14bd
FrankT79 <tacsikf@gmail.com>
2025-02-25 19:28:53 +0100

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import Footer from '../../components/Footer/Footer'
+import Testimonials from '../../components/Testimonials/Testimonials'
+import About from '../../components/About/About'
+
+import Card from '../../components/Card/Card'
+
+function Home() {
+
+ 
+
+
+  return (
+    <>
+    
+    <Hero></Hero>
+    <Card></Card>
+    <Testimonials></Testimonials>
+    <About></About>
+    </>
+  )
+  
+}
+
+export default Home
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/Meta-capstone",
+  plugins: [react()],
+   test: {
+    setupFiles: 'src/setupTests.js',
+    globals: true,
+    environment: "jsdom",
+     
+  },
+})
